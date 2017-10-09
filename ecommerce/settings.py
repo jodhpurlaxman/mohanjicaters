c66285408f2fb1ee089a828b15a3c441a0aabe6d
IT <lky@kexplorer.in>
2017-10-09 19:55:50 +0530

    staticcdn

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -133,13 +133,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_my_proj"),
 ]
 
-#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 #STATIC_ROOT = "/home/mohanjicaters/mohanjicaters/static_my_proj"
 
 
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
 
 
 
