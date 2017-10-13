ac47dee307c148a120a11a599e0d5dae063398e8
IT <lky@kexplorer.in>
2017-10-13 20:20:35 +0530

    contact page added

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -69,11 +69,7 @@ def about_page(request):
 
 def contact_page(request):
     contact_form = ContactForm(request.POST or None)
-    context = {
-        "title":"Contact",
-        "content":" Welcome to the contact page.",
-        "form": contact_form,
-    }
+
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
     # if request.method == "POST":
@@ -81,7 +77,7 @@ def contact_page(request):
     #     print(request.POST.get('fullname'))
     #     print(request.POST.get('email'))
     #     print(request.POST.get('content'))
-    return render(request, "contact/view.html", context)
+    return render(request, "contact/view.html", )
 
 
 
